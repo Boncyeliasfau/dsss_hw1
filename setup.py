31f4a7b9e9c99d96f46e400784d9ae512f71cf1f
Boncyeliasfau <150677999+Boncyeliasfau@users.noreply.github.com>
2023-11-13 20:41:00 +0100

    2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='boncy',
+      version='0.1',
+      description='mathquiz',
+      url='https://github.com/Boncyeliasfau/dsss_hw1.git',
+      author='maths_quiz',
+      author_email='boncyelias1998@gmail.com',
+      license='MIT',
+      packages=['math_quiz'],
+      zip_safe=False)
